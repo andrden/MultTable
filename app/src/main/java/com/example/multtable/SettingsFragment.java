47a0ebcbbfbaf11f27b126b8009f059c3c19311c
denny <andrden@gmail.com>
2015-07-06 14:25:33 +0300

    added forgotten preferences fragments

---
+++ app/src/main/java/com/example/multtable/SettingsFragment.java
@@ -0,0 +1,13 @@
+package com.example.multtable;
+
+import android.os.Bundle;
+
+public class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
